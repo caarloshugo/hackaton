eb98a6550e666c36d0786594a5a59a43eb8a884d
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-07-15 03:11:55 -0500

    consultas por sexo en movitos de trabajo

--- www/applications/default/views/works.php
+++ www/applications/default/views/works.php
@@ -12,19 +12,71 @@
 	var data = google.visualization.arrayToDataTable([
 	  ['Razón', '2007', '2009'],
 	  ['El hogar necesita de su aportación económica', <?php echo $data["2007"][1];?>, <?php echo $data["2009"][1];?>],
-	  ['El hogar necesita de su trabajo', <?php echo $data["2007"][2];?>, <?php echo (int) $data["2009"][2];?>],
+	  ['El hogar necesita de su trabajo', <?php echo $data["2007"][2];?>, <?php echo $data["2009"][2];?>],
 	  ['Aprender un oficios', <?php echo $data["2007"][3];?>, <?php echo $data["2009"][3];?>],
-	  ['Para pagar su escuela y/o sus propios gasto', <?php echo $data["2007"][5];?>, <?php echo $data["2009"][4];?>],
+	  ['Para pagar su escuela y/o sus propios gasto', <?php echo $data["2007"][4];?>, <?php echo $data["2009"][4];?>],
 	  ['No quiere ir a la escuela', <?php echo $data["2007"][5];?>, <?php echo $data["2009"][5];?>],
 	  ['Otra razón', <?php echo $data["2007"][6];?>, <?php echo $data["2009"][6];?>],
 	]);
 
 	var options = {
-	  title: 'Gráfica de resultados - Motivos por los que trabaja',
+	  title: 'Gráfica de resultados - Motivos por los que trabaja (General)',
 	  hAxis: {title: 'Razón',  titleTextStyle: {color: 'red'}}
 	};
 
 	var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+  }
+</script>
+
+<div id="chart_div2" style=" width:750px; height: 500px;"></div>
+    
+<script type="text/javascript">
+  google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(drawChart);
+  function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+	  ['Razón', '2007', '2009'],
+	  ['El hogar necesita de su aportación económica', <?php echo $data["f"]["2007"][1];?>, <?php echo $data["f"]["2009"][1];?>],
+	  ['El hogar necesita de su trabajo', <?php echo $data["f"]["2007"][2];?>, <?php echo $data["f"]["2009"][2];?>],
+	  ['Aprender un oficios', <?php echo $data["f"]["2007"][3];?>, <?php echo $data["f"]["2009"][3];?>],
+	  ['Para pagar su escuela y/o sus propios gasto', <?php echo $data["f"]["2007"][4];?>, <?php echo $data["f"]["2009"][4];?>],
+	  ['No quiere ir a la escuela', <?php echo $data["f"]["2007"][5];?>, <?php echo $data["f"]["2009"][5];?>],
+	  ['Otra razón', <?php echo $data["f"]["2007"][6];?>, <?php echo $data["f"]["2009"][6];?>],
+	]);
+
+	var options = {
+	  title: 'Gráfica de resultados - Motivos por los que trabaja (Niñas)',
+	  hAxis: {title: 'Razón',  titleTextStyle: {color: 'red'}}
+	};
+
+	var chart = new google.visualization.AreaChart(document.getElementById('chart_div2'));
+	chart.draw(data, options);
+  }
+</script>
+
+<div id="chart_div3" style=" width:750px; height: 500px;"></div>
+    
+<script type="text/javascript">
+  google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(drawChart);
+  function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+	  ['Razón', '2007', '2009'],
+	  ['El hogar necesita de su aportación económica', <?php echo $data["m"]["2007"][1];?>, <?php echo $data["m"]["2009"][1];?>],
+	  ['El hogar necesita de su trabajo', <?php echo $data["m"]["2007"][2];?>, <?php echo $data["m"]["2009"][2];?>],
+	  ['Aprender un oficios', <?php echo $data["m"]["2007"][3];?>, <?php echo $data["m"]["2009"][3];?>],
+	  ['Para pagar su escuela y/o sus propios gasto', <?php echo $data["m"]["2007"][4];?>, <?php echo $data["m"]["2009"][4];?>],
+	  ['No quiere ir a la escuela', <?php echo $data["m"]["2007"][5];?>, <?php echo $data["m"]["2009"][5];?>],
+	  ['Otra razón', <?php echo $data["m"]["2007"][6];?>, <?php echo $data["m"]["2009"][6];?>],
+	]);
+
+	var options = {
+	  title: 'Gráfica de resultados - Motivos por los que trabaja (Niños)',
+	  hAxis: {title: 'Razón',  titleTextStyle: {color: 'red'}}
+	};
+
+	var chart = new google.visualization.AreaChart(document.getElementById('chart_div3'));
 	chart.draw(data, options);
   }
 </script>
